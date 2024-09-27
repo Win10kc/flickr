fc8004cee7438b61e28a237dd03f23d2e3aad056
Win10kc <minhht.ba12-126@st.usth.edu.vn>
2024-09-27 08:50:39 +0700

    fix merge error

--- Flickr/app/src/main/java/com/example/flickr/fragment/MenuFragment.java
+++ Flickr/app/src/main/java/com/example/flickr/fragment/MenuFragment.java
@@ -1,7 +1,6 @@
 package com.example.flickr.fragment;
 
 import android.os.Bundle;
-<<<<<<< HEAD:Flickr/app/src/main/java/com/example/flickr/fragment/MenuFragment.java
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,27 +13,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.flickr.R;
 
 import java.util.List;
-=======
-
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
-import com.example.flickr.R;
-
->>>>>>> origin/BI11-183:app/src/main/java/com/example/flickr/fragment/MenuFragment.java
 
 
 public class MenuFragment extends Fragment {
 
-<<<<<<< HEAD:Flickr/app/src/main/java/com/example/flickr/fragment/MenuFragment.java
     private RecyclerView recyclerView;
 
     private List<MenuItem> menuItems;
@@ -46,13 +28,6 @@ public class MenuFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-=======
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the fragment_search layout
-        View view = inflater.inflate(R.layout.fragment_menu, container, false);
-
->>>>>>> origin/BI11-183:app/src/main/java/com/example/flickr/fragment/MenuFragment.java
 
         return view;
     }
