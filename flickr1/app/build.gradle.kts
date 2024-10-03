306b6c48935ad6e77aecfff63ee93c7fea6d835c
yennguyen <cuongnd.ba12-034@st.usth.edu.vn>
2024-10-03 13:12:18 +0700

    new upload 3/10/2024 of Cuong

--- flickr1/app/build.gradle.kts
+++ flickr1/app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -45,4 +44,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    // Add CircleImageView library
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.8.0")
 }
